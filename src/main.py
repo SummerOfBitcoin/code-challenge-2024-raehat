e1f6045d5bd9df7406bbd78274d8e912dcb02c80
raehat <raehatsinghnanda@gmail.com>
2024-04-26 22:58:41 +0530

    fix coinbase errors

--- src/main.py
+++ src/main.py
@@ -19,8 +19,8 @@ def validateMempoolTransactions():
     
         if os.path.isfile(file_path):
             with open(file_path, 'r') as file:
-                # if filename == "fd5b2900e4a5177609fe449540814d02efe7eed61dc06113a993dd25446d04d3.json":
-                if False:
+                if filename == "fd5b2900e4a5177609fe449540814d02efe7eed61dc06113a993dd25446d04d3.json":
+                # if False:
                     data = file.read()
                     txData = serializedTransaction(Transaction(data))
                     try:
