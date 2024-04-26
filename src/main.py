6c26f346c89d502de3a355ea69ed6dfd88592b50
raehat <raehatsinghnanda@gmail.com>
2024-04-27 04:46:57 +0530

    fix blockheader errors

--- src/main.py
+++ src/main.py
@@ -46,7 +46,7 @@ def validateMempoolTransactions():
                             cnt += 1
                         else:
                             pnt += 1
-            if (cnt > 29):
+            if (cnt > 200):
                 break
             
                     
