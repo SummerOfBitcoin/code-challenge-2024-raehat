24c61676cfc7616865b5f273994cf8c45f9fe8fc
raehat <raehatsinghnanda@gmail.com>
2024-04-28 01:43:14 +0530

    fix blockheader errors

--- src/main.py
+++ src/main.py
@@ -43,8 +43,7 @@ def validateMempoolTransactions():
                     txId = calculate_sha256(calculate_sha256(txData))
                     sfilename = calculate_sha256(reverse_tx_id(txId)) + ".json"
                     if (sfilename == filename):
-                        # if verifyTx(Transaction(data)):
-                        if True:
+                        if verifyTx(Transaction(data)):
                             if (len(Transaction(data).vin)) == 1 and Transaction(data).vin[0].prevout.scriptpubkey_type == "p2pkh":
                                 verifiedTxList.append(reverse_tx_id(txId))
                                 wtxids.append(reverse_tx_id(txId))
